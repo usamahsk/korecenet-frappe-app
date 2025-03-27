dc38d67d53b818eb897c611d69f4894b2c562298
Usama Khatri <usamaofficial18@gmail.com>
2025-03-27 18:54:05 +0000

    refactor

--- interview/interview/doctype/add_group_users/add_group_users.py
+++ interview/interview/doctype/add_group_users/add_group_users.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class eval(Document):
+class AddGroupUsers(Document):
 	pass
---
+++ interview/interview/doctype/add_group_users/test_add_group_users.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, usama and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAddGroupUsers(FrappeTestCase):
+	pass
